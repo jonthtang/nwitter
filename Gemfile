d17836c35f5d9a1ee96f0c3d1c8e5e94ea1d51b0
jonthtang <jonthtang@gmail.com>
2017-11-30 09:34:29 +0800

    Added FB log-in with username

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jquery-rails'
 gem 'carrierwave', '~> 1.0'
 gem 'best_in_place', '~> 3.0.1'
 gem 'devise'
+gem 'omniauth-facebook'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
   end
 
   def after_sign_in_path_for(resource)
